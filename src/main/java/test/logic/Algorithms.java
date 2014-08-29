4ff547fe3ef7dca6554952902305db0b3db96b11
Timofey Asyrkin <timofey.asyrkin@zalando.de>
2014-08-29 22:56:13 +0200

    Documented problem 5

--- src/main/java/test/logic/Algorithms.java
+++ src/main/java/test/logic/Algorithms.java
@@ -28,16 +28,26 @@ public class Algorithms {
     }
 
     /**
-     * <p>Problem 5: the number of different routes from START to FINISH vertex with a distance of less than X.
+     * <p>Problem 5: the number of different routes from START vertex to FINISH vertex with a distance of less than
+     * MAX_DISTANCE.
      *
-     * <p>asdad
+     * <p>This problem is NP-hard since it is possible to make a reduction from longest path problem to it in polynomial
+     * time.
+     *
+     * <p>The solution is based on dynamic programming where the state is dp[current_vertex][w] - number of paths to
+     * FINISH vertex from current_vertex with path weight less than or equal to w.
+     *
+     * <p>Algorithm starts from FINISH vertex and traverses all paths with breadth-first search until the path weight is
+     * less or equal than MAX_DISTANCE.
+     *
+     * <p>The result is found by counting dp[START][w], where w < MAX_DISTANCE
      */
-    public static int findNumberOfRoutesWithDistance(final Graph graph, final int from, final int to,
+    public static int findNumberOfRoutesWithDistance(final Graph graph, final int start, final int finish,
             final int maxDistance) {
 
         final Queue<NodeInQueue> queue = new LinkedList<NodeInQueue>();
 
-        queue.add(new NodeInQueue(to, 0));
+        queue.add(new NodeInQueue(finish, 0));
 
         final int[][] dp = new int[graph.getVertexes().size()][maxDistance + 1];
 
@@ -49,7 +59,7 @@ public class Algorithms {
                 if (endEdge.getCost() + node.getDistance() <= maxDistance) {
                     NodeInQueue newNode = new NodeInQueue(endEdge.getVertex(), endEdge.getCost() + node.getDistance());
                     dp[newNode.getVertex()][newNode.getDistance()] +=
-                        dp[node.getVertex()][node.getDistance()] == 0 && node.getVertex() == to
+                        dp[node.getVertex()][node.getDistance()] == 0 && node.getVertex() == finish
                         ? 1 : dp[node.getVertex()][node.getDistance()];
                     queue.offer(newNode);
                 }
@@ -59,7 +69,7 @@ public class Algorithms {
         int result = 0;
 
         for (int distance = 0; distance < maxDistance; distance++) {
-            result += dp[from][distance];
+            result += dp[start][distance];
         }
 
         return result;
